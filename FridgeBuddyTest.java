5c1fa371d1e3e7f3633d325070880ab4881c0c58
jmapa26 <jacob.mapa01@gmail.com>
2026-09-24 08:14:26 -0400

    Created and started on FridgeBuddyTest

---
+++ FridgeBuddyTest.java
@@ -0,0 +1,87 @@
+import student.TestCase;
+
+// -------------------------------------------------------------------------
+/**
+ * Test class for FridgeBuddy
+ * 
+ * @author Jacob Mapa (jmapa26)
+ * @version Sep 24, 2026
+ */
+public class FridgeBuddyTest extends TestCase {
+    // ~ Fields ................................................................
+    private FridgeBuddy buddy;
+    
+    // ~ Constructors ..........................................................
+
+    // ~Public Methods ........................................................
+    /**
+     * Set up method, empty for now.
+     */
+    public void setUp() {
+        buddy = new FridgeBuddy();
+    }
+    
+    /**
+     * Tests main
+     */
+    public void testMain() {
+        
+    }
+    
+    /**
+     * Tests searchFood
+     */
+    public void testSearchFood() {
+        buddy.searchFood();
+        
+    }
+    
+    /**
+     * Tests displayFood
+     */
+    public void testDisplayFood() {
+        
+    }
+    
+    /**
+     * Tests addFood
+     */
+    public void testAddFood() {
+        
+    }
+    
+    /**
+     * Tests removeFood
+     */
+    public void testRemoveFood() {
+        
+    }
+    
+    /**
+     * Tests printExpiredFood
+     */
+    public void testPrintExpiredFood() {
+        
+    }
+    
+    /**
+     * Tests readFoodFile
+     */
+    public void testReadFoodFile() {
+        
+    }
+    
+    /**
+     * Tests writeFoodFile
+     */
+    public void testWriteFoodFile() {
+        
+    }
+    
+    /**
+     * Tests testIsValidDate
+     */
+    public void testIsValidDate() {
+        
+    }
+}

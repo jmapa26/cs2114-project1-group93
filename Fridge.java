e622b9f105d710430c6fba4a9ab8d5b6eaf4f134
ksmith082026S <ksmith08@vt.edu>
2026-09-24 13:13:59 -0400

    formatting

--- Fridge.java
+++ Fridge.java
@@ -1,72 +1,54 @@
-// import statements here
-
-// -----------------------------------------------------------------------------
 /**
- * Stores information about a Fridge (bag) and the food objects insaide of it
+ * Stores information about a Fridge (bag) and the food objects inside of it
  * 
  * @author Katie Smith (ksmith08)
  * @version Sep 24, 2026
  */
 
-public class Fridge implements Bag<Food>
+public class Fridge
+    implements Bag<Food>
 {
 
-    // ~ Fields ................................................................
     private Food[] food;
     private int size;
     private int capacity;
 
     private static final int DEFAULT_SIZE = 10;
 
-    // ~ Constructor ...........................................................
     /**
      * Constructor, creates a Fridge object
      */
-    public Fridge() 
+    public Fridge()
     {
         this(DEFAULT_SIZE);
     }
-    
+
+
     /**
-     * Constructor, creates a Fridge object
+     * Constructor, creates a Fridge object with an inputted capactiy
      * 
-     * @param  capacity number of items that the bag can hold
+     * @param capacity
+     *            number of items that the bag can hold
      */
     @SuppressWarnings("unckecked")
     public Fridge(int capacity)
     {
-        this.food = (Food[]) new Object[capacity];
+        this.food = (Food[])new Object[capacity];
         this.capacity = capacity;
         size = 0;
     }
 
-    // ~ Public Methods ........................................................
-
-    /*
-     * Please add javadoc comments for all your methods,
-     * even if they are self-explanatory!
-     *
-     * For javadoc comments, you can use the following tags
-     * 
-     * @param parameter
-     * Include parameter explanation
-     * 
-     * @return return value, and explanation if necessary
-     * 
-     * @throws exceptionName
-     * if the method throws an exception
-     */
 
     /**
      * Adds a food item to the Fridge
      * 
-     * @param  newFood the new food item to be added
-     * 
+     * @param newFood
+     *            the new food item to be added
      * @return boolean whether or not the food was successfully added
      */
-    public boolean add(Food newFood) 
+    public boolean add(Food newFood)
     {
-        if(size >= capacity)
+        if (size >= capacity)
         {
             return false;
         }
@@ -74,24 +56,24 @@ public class Fridge implements Bag<Food>
         size++;
         return true;
     }
-    
-    
+
+
     /**
      * removess a speciic food item from the Fridge
      * 
-     * @param  toBeRemoved the food item to be removed
-     * 
+     * @param toBeRemoved
+     *            the food item to be removed
      * @return Food the food item that was removed from the Fridge
      */
     public Food remove(Food toBeRemoved)
     {
-        if(size == 0)
+        if (size == 0)
         {
             return null;
         }
-        for(int i = 0; i < size; i++)
+        for (int i = 0; i < size; i++)
         {
-            if(food[i].equals(toBeRemoved))
+            if (food[i].equals(toBeRemoved))
             {
                 food[i] = null;
                 size--;
@@ -100,39 +82,40 @@ public class Fridge implements Bag<Food>
         }
         return toBeRemoved;
     }
-    
-    
+
+
     /**
      * Clears every item out of the Fridge
      */
     public void clear()
     {
-        for(int i = 0; i < size; i++)
+        for (int i = 0; i < size; i++)
         {
             food[i] = null;
         }
     }
-    
+
+
     /**
      * Checks if the Fridge contains the inputted Food item
      * 
-     * @param  String name of the food that is being searched for
-     * 
+     * @param String
+     *            name of the food that is being searched for
      * @return boolean whether or not the Fridge contains that Food
      */
     public boolean contains(String anEntry)
     {
-        for(int i = 0; i < size; i++)
+        for (int i = 0; i < size; i++)
         {
-            if(food[i].getName().equals(anEntry))
+            if (food[i].getName().equals(anEntry))
             {
                 return true;
             }
         }
         return false;
     }
-    
-    
+
+
     /**
      * Checks if the Fridge is empty
      * 
@@ -140,35 +123,35 @@ public class Fridge implements Bag<Food>
      */
     public boolean isEmpty()
     {
-        if(size == 0)
+        if (size == 0)
         {
             return true;
         }
         return false;
     }
-    
-    
+
+
     /**
      * checks the number of times a certain food is in the Fridge
      * 
-     * @param  String the name of the Food that is being counted
-     * 
+     * @param String
+     *            the name of the Food that is being counted
      * @return int the number of times that Food is in the Fridge
      */
     public int getFrequency(String anEntry)
     {
         int count = 0;
-        for(int i = 0; i < size; i++)
+        for (int i = 0; i < size; i++)
         {
-            if(food[i].getName().equals(anEntry))
+            if (food[i].getName().equals(anEntry))
             {
                 count++;
             }
         }
         return count;
     }
-    
-    
+
+
     /**
      * returns the number of items currently in the Fridge
      * 
@@ -178,8 +161,8 @@ public class Fridge implements Bag<Food>
     {
         return size;
     }
-    
-    
+
+
     /**
      * Turns the Foods in the Fridge into one string
      * 
@@ -187,21 +170,21 @@ public class Fridge implements Bag<Food>
      */
     public String toString()
     {
-        if(size == 0)
+        if (size == 0)
         {
             return "[]";
         }
-        
+
         String foodString = "[";
-        for(int i = 0; i < size - 1; i++)
+        for (int i = 0; i < size - 1; i++)
         {
             foodString += food[i] + ", ";
         }
         foodString += food[size - 1] + "]";
         return foodString;
     }
-    
-    
+
+
     /**
      * Turns the Foods in the Fridge into an array
      * 
@@ -210,15 +193,11 @@ public class Fridge implements Bag<Food>
     public Food[] toArray()
     {
         Food[] foodArr = new Food[size];
-        for(int i = 0; i < size; i++)
+        for (int i = 0; i < size; i++)
         {
             foodArr[i] = food[i];
         }
         return foodArr;
     }
-    
-    
-    
-    
-}
 
+}
--- FridgeTest.java
+++ FridgeTest.java
@@ -4,35 +4,36 @@
  * @author Katie Smith (ksmith08)
  * @version Sep 24, 2026
  */
-public class FridgeTest extends student.TestCase
+public class FridgeTest
+    extends student.TestCase
 {
     private Fridge fridge1;
     private Fridge fridge2;
-    
+
     public void setUp()
     {
         fridge1 = new Fridge();
         fridge2 = new Fridge(5);
-        
+
         Food apple = new Food("Apple", today.plusDays(2));
         Food sandwich = new Food("Sandwich", today.plusDays(3));
         Food milk = new Food("Milk", today.plusDays(1));
-        
+
         fridge1.add(apple);
         fridge2.add(sandwich);
         fridge1.add(milk);
-        
+
         Food tomato = new Food("Tomato", today.plusDays(3));
         Food butter = new Food("Butter", today.plusDays(1));
-        
+
         fridge1.add(apple);
         fridge2.add(sandwich);
         fridge2.add(milk);
         fridge2.add(tomato);
         fridge2.add(butter);
     }
-    
-    
+
+
     /**
      * tests add()
      */
@@ -41,11 +42,11 @@ public class FridgeTest extends student.TestCase
         Food juice = new Food("Juice", today.plusDays(2));
         assertTrue(fridge1.add(juice));
         assertEquals(4, fridge1.getCurrentSize());
-        
+
         assertFalse(fridge2.add(juice));
     }
-    
-    
+
+
     /**
      * tests remove()
      */
@@ -53,11 +54,11 @@ public class FridgeTest extends student.TestCase
     {
         Fridge empty = new Fridge();
         assertNull(empty.remove(apple));
-        
+
         assertEquals(sandwich, fridge1.remove(sandwich));
     }
-    
-    
+
+
     /**
      * tests clear()
      */
@@ -65,10 +66,10 @@ public class FridgeTest extends student.TestCase
     {
         fridge1.clear();
         assertEquals(0, fridge1.getCurrentSize());
-        
+
     }
-    
-    
+
+
     /**
      * tests contains()
      */
@@ -77,8 +78,8 @@ public class FridgeTest extends student.TestCase
         assertTrue(fridge2.contains("Tomato"));
         assertFalse(fridge2.contains("Mustard"));
     }
-    
-    
+
+
     /**
      * tests isEmpty()
      */
@@ -88,23 +89,23 @@ public class FridgeTest extends student.TestCase
         assertTrue(empty.isEmpty());
         assertFalse(fridge1.isEmpty());
     }
-    
-    
+
+
     /**
      * tests getFrequency()
      */
     public void testGetFrequency()
     {
         assertEquals(1, fridge2.getFrequency("Apple"));
-        
+
         Food sandwich = new Food("Sandwich", today.plusDays(3));
         fridge2.add(sandwich);
         fridge2.add(sandwich);
-        
+
         assertEquals(3, fridge1.getFrequency("Sandwich"));
     }
-    
-    
+
+
     /**
      * tests getCurrentSize()
      */
@@ -112,8 +113,8 @@ public class FridgeTest extends student.TestCase
     {
         assertEquals(3, fridge1.getCurrentSize());
     }
-    
-    
+
+
     /**
      * tests toString()
      */
@@ -121,12 +122,12 @@ public class FridgeTest extends student.TestCase
     {
         String fridge1String = "[Apple, Sandwich, Milk]";
         assertEquals(fridge1String, fridge1.toString());
-        
+
         Fridge empty = new Fridge();
         assertEquals("[]", empty.toString());
     }
-    
-    
+
+
     /**
      * tests toArray()
      */
@@ -135,8 +136,8 @@ public class FridgeTest extends student.TestCase
         Food apple = new Food("Apple", today.plusDays(2));
         Food sandwich = new Food("Sandwich", today.plusDays(3));
         Food milk = new Food("Milk", today.plusDays(1));
-        
-        Food[] arr = {apple, sandwich, milk};
+
+        Food[] arr = { apple, sandwich, milk };
         assertEquals(arr, fridge1.toArray());
     }
 
